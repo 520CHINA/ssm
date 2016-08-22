f39fa66c95d5885129e53c5f7cccac5ba229535a
jange.zhang@gmail.com <jange.zhang@gmail.com>
2016-08-23 05:01:57 +0800

    dsfds

--- src/main/java/com/soecode/lyf/web/BookController.java
+++ src/main/java/com/soecode/lyf/web/BookController.java
@@ -17,9 +17,13 @@ import com.soecode.lyf.dto.AppointExecution;
 import com.soecode.lyf.dto.Result;
 import com.soecode.lyf.entity.Book;
 import com.soecode.lyf.service.BookService;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+
+
 
 @Controller
-@RequestMapping("/book") // url:/模块/资源/{id}/细分 /seckill/list
+@RequestMapping("/book/") // url:/模块/资源/{id}/细分 /seckill/list
 public class BookController {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -27,18 +31,34 @@ public class BookController {
 	@Autowired
 	private BookService bookService;
 
-	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	@RequestMapping(value = "/list", method = RequestMethod.GET , produces = {"application/json; charset=utf-8"})
+	@ResponseBody
 	private String list(Model model) {
 		List<Book> list = bookService.getList();
 		model.addAttribute("list", list);
 		// list.jsp + model = ModelAndView
 		return "list";// WEB-INF/jsp/"list".jsp
 	}
+	
+	
+	@RequestMapping(value = "/test/v1", method = RequestMethod.GET)
+	@ResponseBody
+	private String list1(Model model) {
+		return "test1";
+	}
+	
+	
+	@RequestMapping(value = "/test/v2", method = RequestMethod.GET)
+	@ResponseBody
+	private String list2(Model model) {
+		return "list2";
+	}
 
 	// ajax json
-	@RequestMapping(value = "/{bookId}/detail", method = RequestMethod.GET)
+	@RequestMapping(value = "/{bookId}/detail", method = RequestMethod.GET , produces = {"application/json; charset=utf-8"} )
 	@ResponseBody
-	private String detail(@PathVariable("bookId") Long bookId, Model model) {
+	@ApiOperation(value = "根据id获取图书信息", httpMethod = "GET", response = String.class, notes = "根据id获取图书信息")
+	private String detail(@ApiParam(required = true, name = "bookId", value = "书id") @PathVariable("bookId") Long bookId, Model model) {
 		if (bookId == null) {
 			return "redirect:/book/list";
 		}
@@ -47,11 +67,13 @@ public class BookController {
 			return "forward:/book/list";
 		}
 		model.addAttribute("book", book);
-		return "detail";
+		
+		//JSONObject json = JSONObject.fromObject(model);
+		System.out.print(book.getName());
+		return book.getName();
 	}
 
-	@RequestMapping(value = "/{bookId}/appoint", method = RequestMethod.POST, produces = {
-			"application/json; charset=utf-8" })
+	@RequestMapping(value = "/{bookId}/appoint", method = RequestMethod.POST, produces = {"application/json; charset=utf-8" })
 	private Result<AppointExecution> appoint(@PathVariable("bookId") Long bookId, @Param("studentId") Long studentId) {
 		if (studentId == null || studentId.equals("")) {
 			return new Result<>(false, "学号不能为空");
--- src/test/java/com/soecode/lyf/BaseTest.java
+++ src/test/java/com/soecode/lyf/BaseTest.java
@@ -1,15 +1,62 @@
 package com.soecode.lyf;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-/**
- * 配置spring和junit整合，junit启动时加载springIOC容器 spring-test,junit
- */
+import com.soecode.lyf.entity.Book;
+import com.soecode.lyf.service.BookService;
+import com.soecode.lyf.web.BookController;
+
+
 @RunWith(SpringJUnit4ClassRunner.class)
-// 告诉junit spring配置文件
+@WebAppConfiguration
+
 @ContextConfiguration({ "classpath:spring/spring-dao.xml", "classpath:spring/spring-service.xml" })
 public class BaseTest {
+	
+	private MockMvc mockMvc;
+	
+	@Mock
+	private BookService bookService;
+	
+	@InjectMocks
+	private BookController bookController = new BookController();
+	
+	
+	@Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(bookController).build();
+    }
+	
+	@Test
+	public void testDetail() throws Exception {
+		
+		when(bookService.getById(1003)).thenReturn(new Book(1003,"编译原理",10));
 
+        mockMvc.perform(get("/book/1003/detail"))
+                .andDo(print())
+                .andExpect(status().isOk()).andExpect(content().string(is("编译原理")));
+
+        verify(bookService).getById(1003);
+	}
 }
